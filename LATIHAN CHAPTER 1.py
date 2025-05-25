43745e4656ba98ae1575b0fad2a0de02e8e1c828
Denny Risnandar <dennyrisnandar@gmail.com>
2025-05-25 22:29:29 +0700

    Add files via upload

---
+++ LATIHAN CHAPTER 1.py
@@ -0,0 +1,87 @@
+#print("Halo Dunia! Ayo kurangi sampah plastik bareng Arry Hutomo!")
+#print('Halo Dunia! Matikan lampu kalo ga dipake, saran Arry Hutomo!')
+#pesan = 'halo dunia! ayo tanam pohon bareng denny risnandar'
+#print(pesan)
+
+#print ("halo Dunia" + 'ayo bantu anak putus sekolah bareng denny risnandar')
+
+#nomor 5
+#nama = "denny risnandar" 
+#pesan = 'halo indonesia! ayo dukung UMKM lokal baraeng'
+#print (f'{pesan} {nama}')
+
+
+#nomor 6
+#print("Halo Dunia! {} kurangi polusi udara bareng {}!".format("Ayo", "Arry Hutomo"))
+
+#print ('halo indonesia {} kurangi polusi udara bareng {} {}'.format("ayo", "denny risnandar", "dan jayendra"))
+
+
+
+#Nomor 7
+#print("Ayo hemat air bareng Arry Hutomo! " * 3)
+
+#print('ayo rajin menabung emas bareng denny risnandar, ' * 5)
+
+#nomor 8
+#print("Halo Dunia!\nAyo donasi buku bareng Arry Hutomo!")
+
+#print ('halo indonesia\n ayo menabung emas')
+
+#nomor 9
+#print("Halo Dunia! Ayo beli produk lokal ", end="")
+#print("bareng Arry Hutomo!")
+
+#print("Halo indonesia, ayo menabung emas", end="")
+#print("bareng kang denny")
+
+#nomor 10
+#print("Halo Dunia", "Ayo daur ulang sampah bareng Arry Hutomo!", sep=" - ")
+
+#print('halo indonesia', 'ayo menabung emas dengan denny risnandar', sep=' - ')
+
+
+#nomor 11
+#nama = "Arry Hutomo"
+#kegiatan = "kurangi emisi karbon"
+#print(f"Halo Dunia, {kegiatan}", f"bareng {nama}!", sep=" => ")
+
+
+#nama  = 'denny risnandar'
+#aktivitas = 'sosialisasi perbankan syariah'
+#print(f'halo indoneisa, {aktivitas}', f'bersama ahlinya {nama}')
+
+
+#nomor 12
+#print("Halo Dunia!\nAyo hemat listrik ", end="")
+#("bareng Arry Hutomo!")
+
+#print('halo indonesia\nayo rajin menabung emas ' , end='')
+#print('bareng denny risnandar')
+
+
+#nomor 13
+
+#nama = "Arry Hutomo"
+#print(f"Ayo donasi buku bareng {nama}! " * 2)
+
+#nama = 'Denny Risnandar'
+#print(f'ayo kita rajin sedekah bareng {nama} ! '  * 3)
+
+
+#nomor 14
+#print("Halo Dunia!\n{} dukung UMKM bareng {}!".format("Ayo", "Arry Hutomo"))
+
+
+#print("halo indonesia\n{} dukung perbankan syariah bareng {} insya allah ekonomi kuat {}!". format("mari", "denny risnandar", "bismillah"))
+
+#nomor 15
+#nama = "Arry Hutomo"
+#kegiatan = "daur ulang sampah"
+#print(f"Halo Dunia!\n{kegiatan} ", f"bareng {nama} " * 2, sep=" => ", end="")
+#print("yuk!")
+
+nama = 'denny risnandar'
+kegiatan = 'sosialiasi perbankan syariah dengan pemanfaatan dunia ai '
+print(f'halo indonesia\n{kegiatan}', f' bareng {nama},' * 3, sep='--', end='')
+
