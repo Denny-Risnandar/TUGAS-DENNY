0ccd43fb4c707e3a74823cc934fb7f66f68c15d5
Denny Risnandar <dennyrisnandar@gmail.com>
2025-05-30 20:54:31 +0700

    Add files via upload

---
+++ LATIHAN CHAPTER 2.py
@@ -0,0 +1,120 @@
+
+#nomor 1
+#jumlah_pohon = 50
+#print(f"Arry Hutomo tanam {jumlah_pohon} pohon!")
+
+#jumlah_Emas = 50
+#print (f'denny risnandar perlu (jumlah_Emas) emas!')
+
+
+#Nomor 2
+#berat_sampah = 12.5
+#print(f"Arry Hutomo daur ulang {berat_sampah} kg sampah!")
+
+#Berat_Emas = 20.5
+#print(f'denny perlu {Berat_Emas} gr!')
+
+
+#Nomor 3
+#Hemat_Energi = False
+#print(f'denny bilang : Boros Listrik itu {Hemat_Energi}')
+
+#Nomor 4
+#komunitas = "ECC Team"
+#print(f"Arry Hutomo bareng {komunitas} bantu anak putus sekolah!")
+
+#Komunitas = 'Denny BSI'
+#print(f'Indonesia bareng {Komunitas}  bantu kebutuhan keuangan masyarakat')
+
+#Nomor 5
+#produk = "Kerajinan Tangan"
+#penjual = "Ibu Siti"
+#print(f"Arry Hutomo dukung {penjual} jual {produk}!")
+
+#Nomor 6
+#donasi = 500000
+#print(f"Arry Hutomo donasi Rp {donasi} buat anak yatim!")
+
+#Harga_Emas = 1900000
+#print(f' denny beli emas 1 gr seharga Rp {Harga_Emas} buat pa Arry')
+
+#Nomor 7 Nyimpen Presentase Emisi
+#persentase_emisi = 25.5
+#print(f"Arry Hutomo kurangi emisi {persentase_emisi}%!")
+
+#Persentase_HDE = 50.5
+#print(f'Kenaikan harga emas tahun 2025 sudah tembus {Persentase_HDE} %')
+
+#8. Nyimpen Status Daur Ulang (Menantang)
+#daur_ulang = False
+#print(f"Arry Hutomo tanya: Sudah daur ulang belum? {daur_ulang}")
+
+#jual_emas = False
+#print(f'denny tanya : sudah jual emas belum : {jual_emas}')
+
+#9. Kombinasi f-String dan Chapter 1 (Menantang)
+#pesan = "Ayo hemat listrik"
+#nama = "Arry Hutomo"
+#print(f"{pesan} bareng {nama}!\nLakukan sekarang!")
+
+#pesan = "Ayo rajin menabung emas"
+#nama = 'Denny'
+#print(f'{pesan} bareng {nama} \nLakukan mulai dari yang kecil')
+
+#10. Pake .format() dari Chapter 1 (Menantang)
+
+#komunitas = "ECC Team"
+#print("Arry Hutomo bareng {} bantu UMKM!".format(komunitas))
+
+#komunitas = 'pecinta emas'
+#print('denny bareng {} cegah inflasi'.format(komunitas))
+
+#11, gabungin dua tipe data
+#jumlah = 100
+#barang = "buku"
+#print(f'ari hutomo donasi {jumlah} {barang} buat anak yatim')
+
+#jumlah = 1
+#satuan = 'kg'
+#barang = 'emas'
+#print(f'denny membeli {barang} seberat {jumlah} {satuan}')
+
+#12. Pake Separator dari Chapter 1 (Sulit)
+#pesan = "Ayo tanam pohon"
+#nama = "Arry Hutomo"
+##print(pesan, nama, sep=" bareng ")
+
+#pesan = "ayo beli emas"
+#nama = "di antam"
+#print(pesan, nama, sep=' tapi ')
+
+#13. Kombinasi End dan f-String (Sulit)
+#jumlah = 20.5
+#print(f"Arry Hutomo daur ulang {jumlah} kg sampah ", end="")
+#print("keren!")
+
+#jumlah = 10
+#print(f'denny membeli emas {jumlah} kg emas ', end='')
+#print(f'mantap')
+
+#14. Kombinasi Semua dari Chapter 1 (Lebih Sulit)
+#nama = "Arry Hutomo"
+#print(f"Ayo hemat energi bareng {nama}!\nLakukan ", end="")
+#print("sekarang!")
+
+#nama = 'denny'
+#print(f'ayo rajin menabung emas bareng {nama} \nLakukan ', end='')
+#print(f'dari saat ini')
+
+#15. Semua Digabung (Paling Sulit)
+#jumlah = 300
+#barang = "pohon"
+#nama = "Arry Hutomo"
+#print(f"Arry Hutomo tanam {jumlah} {barang} ", f"bareng {nama}!\nAyo ikut ", sep="=>", end="")
+#print("sekarang!")
+
+#jumlah = 10
+#barang = "emas"
+#nama = 'denny'
+#print(f'denny membeli {barang} {jumlah} ', f'bareng {nama}\nayo ikut ', sep='--->', end='')
+#print(f'lakukan sekarang')
---
+++ LATIHAN CHAPTER 3.py
@@ -0,0 +1,177 @@
+#1. Tambah Jumlah Pohon (Super Gampang)
+#pohon_tanam = 50
+#pohon_baru = 30
+#total_pohon = pohon_tanam + pohon_baru
+#print(f"Arry Hutomo tanam {total_pohon} pohon!")
+
+#padi_tanam = 10
+#padi_baru = 100
+#total_padi = padi_tanam + padi_baru
+#print(f'denny telah menanam {total_padi} pohon')
+
+#2. Kurang Sampah (Gampang)
+#sampah_awal = 100
+#sampah_daur_ulang = 40
+#sisa_sampah = sampah_awal - sampah_daur_ulang
+#print(f"Arry Hutomo kurangi sampah jadi {sisa_sampah} kg!")
+
+#padi_awal = 100
+#padi_panen = 40
+#sisa_panen = padi_awal - padi_panen
+#print(f'{sisa_panen} - ayo kembali menanam')
+
+#3. Kali Penghematan Energi (Gampang)
+#lampu_per_rumah = 5
+#rumah = 10
+#total_lampu = lampu_per_rumah * rumah
+#print(f"Arry Hutomo hemat {total_lampu} lampu!")
+
+#padi_per_petaksawah = 100
+#petaksawah = 1000
+#total_padi = padi_per_petaksawah * petaksawah
+#print(f'denny hemat {total_padi} padi')
+
+#4. Bagi Donasi (Masih Gampang)
+#total_donasi = 1000000
+#penerima = 5
+#donasi_per_orang = total_donasi / penerima
+#print(f"Arry Hutomo bagi donasi Rp {donasi_per_orang} per orang!")
+
+#total_padi = 10000
+#petani = 20
+#donasi = total_padi / petani
+#print(f'denny berbagi bibit {donasi} padi ke petani - ')
+
+#5. Kombinasi Tambah dan Kurang (Mulai Agak Menantang)
+#pohon_awal = 100
+#pohon_tanam = 50
+#pohon_mati = 20
+#total_pohon = pohon_awal + pohon_tanam - pohon_mati
+#print(f"Arry Hutomo punya {total_pohon} pohon!")
+
+#padi_awal = 3000
+#padi_ditanam = 2400
+#padi_mati = 150
+#total_padi = padi_awal +padi_ditanam -  padi_mati
+#print(f'denny saat ini punya {total_padi}')
+
+#6. Kombinasi Kali dan Bagi (Agak Menantang)
+#sampah_per_orang = 2.5
+#orang = 10
+#total_sampah = sampah_per_orang * orang
+#rata_rata = total_sampah / 2
+#print(f"Arry Hutomo hitung rata-rata sampah {rata_rata} kg!")
+
+#padi_perpetani = 5
+#petani = 100
+#total_padi = padi_perpetani * petani
+#rata_rata = total_padi / 2
+#print(f' denny hitung ada rata rata padi ada {rata_rata}')
+
+#7. Pake Modulus (Agak Menantang)
+#total_donasi = 1000000
+#penerima = 3
+#sisa_donasi = total_donasi % penerima
+#print(f"Arry Hutomo punya sisa donasi Rp {sisa_donasi}!")
+
+#total_padi = 10
+#penerima = 4
+#sisa_donasi = total_padi % penerima
+#print(f' denny punya sisa donasi Rp {sisa_donasi}')
+
+#8. Pake Floor Division (Menantang)
+#total_lampu = 25
+#rumah = 4
+#lampu_per_rumah = total_lampu // rumah
+#print(f"Arry Hutomo bagi {lampu_per_rumah} lampu per rumah!")
+
+#total_padi = 60
+#sawah = 7
+#padi_persawah = total_padi // sawah
+#print(f'denny bagi {padi_persawah} persawah')
+
+#9. Pake Chapter 1 untuk Tampilan (Menantang)
+#pohon = 50
+#pohon_tambah = 30
+#total = pohon + pohon_tambah
+#print(f"Arry Hutomo tanam {total} pohon!\nAyo ikut!")
+
+#padi = 60
+#padi_tambah = 30
+#total = padi + padi_tambah
+#print(f'denny tanam {total} padi! \nayo gabung')
+
+#10. Kombinasi Operasi dan Separator (Menantang)
+#sampah = 100
+#daur_ulang = 40
+#sisa = sampah - daur_ulang
+#print("Arry Hutomo kurangi", sisa, "kg sampah!", sep=" => ")
+
+#padi_hampirmati = 100
+#padi_tumbuh_kembali = 70
+#sisa = padi_hampirmati - padi_tumbuh_kembali
+#print(f'denny kurangi', sisa, 'bibit padi', sep= "==>>")
+
+#11. Kombinasi Operasi dan End (Agak Sulit)
+#total_lampu = 20
+#rumah = 5
+#lampu_rumah = total_lampu / rumah
+#print(f"Arry Hutomo bagi {lampu_rumah} lampu ", end="")
+#print("per rumah!")
+
+#total_padi = 100
+#sawah = 2
+#padi_sawah = total_padi / sawah
+#print(f'denny bagi {padi_sawah} padi ', end="")
+#print(f'per sawah')
+
+#12. Operasi Kompleks (Sulit)
+#pohon = 100
+#tambah = 50
+#mati = 20
+#rata_rata = (pohon + tambah - mati) / 2
+#print(f"Arry Hutomo hitung rata-rata {rata_rata} pohon!")
+
+#padi = 100
+#tambah = 50
+#padi_kering = 0
+#rata_rata = (padi + tambah - padi_kering) / 2
+#print(f'denny hitung rata-rata {rata_rata} padi')
+
+
+#13. Operasi dengan Modulus dan Chapter 1 (Sulit)
+#donasi = 1000000
+#penerima = 3
+#sisa = donasi % penerima
+#print(f"Arry Hutomo punya sisa Rp {sisa} ", f"setelah bagi donasi!", sep="=>")\
+
+#padi = 1000000
+#petani = 3
+#sisa = padi % petani
+#print(f'denny punya sisa Rp {sisa} ', 'setelah di bagikan', sep="-->>")
+
+#14. Operasi dengan Floor Division dan Chapter 1 (Lebih Sulit)
+#lampu = 25
+#rumah = 4
+#bagi = lampu // rumah
+#print(f"Arry Hutomo bagi {bagi} lampu per rumah!\nAyo hemat!")
+
+#padi = 36
+#sawah = 5
+#bagi = padi // sawah
+#print(f'denny bagi {bagi} padi per sawah,\nAyo berbagi')
+
+
+#15. Semua Operasi Digabung (Paling Sulit)
+#sampah = 100
+#daur = 40
+#tambah = 20
+#hasil = (sampah - daur + tambah) * 2 / 3
+#print(f"Arry Hutomo hitung sampah {hasil} kg!\nLakukan ", f"lagi bareng ECC Team!", sep="=>", end="")
+#print("yuk!")
+
+padi_busuk = 200
+daur_padi = 50
+tambah_padi = 40
+hasil = (padi_busuk - daur_padi + tambah_padi) * 2 / 3
+print(f'denny hitung padi {hasil} kg, \nlakukan ', ' lagi bareng ecc team! yuk!', sep ="--<>", end='')
